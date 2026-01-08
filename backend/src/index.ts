2350fde5e7c4f44ce4db58e4d20272ec028c1b8d
FHZergursh <89704618+FHZergursh@users.noreply.github.com>
2026-01-08 19:10:41 +0000

    setup backend
    Doing most of the architecture that I can before implementing db to make my future life easier.

---
+++ backend/src/index.ts
@@ -0,0 +1,13 @@
+import express from "express"
+import {ENV} from "./config/env"
+
+const app = express()
+
+app.get("/", (req, res) => {
+  res.json({success: true})
+})
+
+
+app.listen(ENV.PORT, () => {
+  console.log("server running on port " + ENV.PORT)
+})
